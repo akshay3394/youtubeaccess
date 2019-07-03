ecce18099741cc651b8348a540e219dd7cf99cdb
Akshay Mhapankar <Akshay.Mhapankar@1eQ.com>
2019-07-03 18:01:41 +0530

    added download logic

--- main.js
+++ main.js
@@ -51,6 +51,23 @@ app.get("/stream", (req, res)=>{
 })
 
 
+app.get("/", (req, res)=>{
+
+    var URL = req.query.q;
+    //var q = req.query.q;
+
+    console.log("query string :"+URL);
+
+    //res.header('Content-Disposition', 'attachment; filename="video.mp4"')
+    res.header("content-type", "video/mp4");
+
+    request.get(URL).pipe(res)
+
+    //res.send("Hello");
+})
+
+
+
 var server = app.listen(port, ()=>{
      console.log("server started at port "+port);
 });
